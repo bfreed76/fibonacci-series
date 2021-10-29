7a5c351d9569630926add3d613b036e4434448a7
Brinton <brintonfoyreed@gmail.com>
2021-10-29 13:46:58 -0700

    Finished

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,11 +1,26 @@
 function fibonacci(num) {
-  // type your code here
+  let index = num - 1;
+  let value = 0;
+  let fib = [0, 1];
+
+  if (num < 1) return 0;
+
+  while (index > 0) {
+    value = fib[fib.length - 1] + fib[fib.length - 2];
+    fib.push(value);
+    index--;
+  }
+  return fib[fib.length - 1];
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: 0");
   console.log("=>", fibonacci(0));
+
+  console.log("");
+
+  console.log("Expect 3");
+  console.log("=>", fibonacci(5));
 
   console.log("");
 
@@ -20,5 +35,13 @@ if (require.main === module) {
 
 module.exports = fibonacci;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/*
+Create FS generator. Compare each iteration to num (-1 as 0,1 is the starting point).
+While index is greater than 0, the value var = the last element plus the second to last
+elment in the fib array. Then, that value is pushed to fib array and returned when the index,
+which is decremented every iteration. Another option is memoization, which is storing value in
+a hash and checking
+*/
+
+// missed "Seq starts with 0 followed by 1"
+// countdown to avoid issues with low parameters as inputs
